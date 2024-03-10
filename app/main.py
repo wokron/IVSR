1e81f7ef31e8bf2183a567ba59ee4281452b34bd
wokron <stringcatwok@gmail.com>
2024-03-10 10:17:08 +0800

    add database

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from app.api import api
+from app.database import create_db_and_tables
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(api)
