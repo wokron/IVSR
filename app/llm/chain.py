45d9b4fac35e5a164cbeabfa1cc885f98a2265b8
wokron <stringcatwok@gmail.com>
2024-03-10 09:30:59 +0800

    add llm

---
+++ app/llm/chain.py
@@ -0,0 +1,54 @@
+from functools import lru_cache
+from typing import Any
+import dotenv
+from langchain_community.llms.baidu_qianfan_endpoint import QianfanLLMEndpoint
+from langchain.chains.summarize import load_summarize_chain
+from langchain_core.runnables import RunnablePick, RunnableSerializable
+from langchain_core.output_parsers import StrOutputParser
+from langchain_text_splitters import RecursiveJsonSplitter
+
+from app.llm.prompts import (
+    ML_PROMPT,
+    REPORT_PROMPT,
+    STATIC_PROMPT,
+    STATIC_REFINE_PROMPT,
+)
+
+
+@lru_cache
+def create_chain():
+    dotenv.load_dotenv(".env")
+
+    llm = QianfanLLMEndpoint(model="Llama-2-13b-chat")
+
+    static_summary_chain = load_summarize_chain(
+        llm,
+        chain_type="refine",
+        question_prompt=STATIC_PROMPT,
+        refine_prompt=STATIC_REFINE_PROMPT,
+        input_key="static_docs",
+    ) | RunnablePick("output_text")
+
+    ml_summary_chain = ML_PROMPT | llm | StrOutputParser()
+
+    report_chain = (
+        {"static_summary": static_summary_chain, "ml_summary": ml_summary_chain}
+        | REPORT_PROMPT
+        | llm
+        | StrOutputParser()
+    )
+
+    return report_chain
+
+
+async def generate_llm_report(
+    chain: RunnableSerializable[Any, str],
+    static_content: str,
+    ml_text: str,
+):
+    static_docs = RecursiveJsonSplitter().create_documents(
+        texts=[static_content], convert_lists=True
+    )
+
+    result = await chain.ainvoke({"static_docs": static_docs, "ml_text": ml_text})
+    return result
---
+++ app/llm/prompts.py
@@ -0,0 +1,85 @@
+from langchain_core.prompts import PromptTemplate
+
+
+STATIC_PROMPT_TEMPLATE = """\
+Write a summary of the following json content:
+------------
+{text}
+------------
+SUMMARY:\
+"""
+STATIC_PROMPT = PromptTemplate.from_template(STATIC_PROMPT_TEMPLATE)
+
+
+STATIC_REFINE_PROMPT_TEMPLATE = """\
+Your job is to produce a final summary.
+We have provided an existing summary up to a certain point:
+------------
+{existing_answer}
+------------
+We have the opportunity to refine the existing summary (only if needed) with some more context below.
+------------
+{text}
+------------
+Given the new context, refine the original summary (and do not mention the original summary again).
+If the context isn't useful, return the original summary.
+FINAL SUMMARY:\
+"""
+STATIC_REFINE_PROMPT = PromptTemplate.from_template(STATIC_REFINE_PROMPT_TEMPLATE)
+
+
+ML_PROMPT_TEMPLATE = """\
+The following content is the result of XmalPlus, an Android malware classifier.
+Your job is to write a concise summary of the following json content:
+------------
+{ml_text}
+------------
+where:
+- `apk_name` is the apk analyzed by XmalPlus.
+- `malware_score` means the possibility that the application is malware. The value range is 0-1.
+- `key_features` are the key features that define the application as a malware.
+- `associate_features`: are other features associated with the key feature.
+CONCISE SUMMARY:\
+"""
+ML_PROMPT = PromptTemplate.from_template(ML_PROMPT_TEMPLATE)
+
+
+REPORT_PROMPT_TEMPLATE = """\
+Your job is to produce a analysis report based on the following content:
+------------
+{static_summary}
+
+{ml_summary}
+------------
+The analysis report should be in markdown format, and include as much information as possible.
+The report should have these sections:
+1. introduction (keep it simple)
+2. reports of different parts of analysis (at least three parts here, exclude conclusion and recommendations)
+3. conclusion
+4. recommendations
+
+For example, the format of your report could by like this:
+ANALYSIS REPORT: YOUR APP NAME
+==============================
+
+Introduction
+------------
+The following analysis...
+
+Part1: XXX
+----------
+The XXX apk requests ..., include
+1. ...
+2. ...
+
+Conclusion
+----------
+...
+
+Recommendations
+---------------
+...
+
+ANALYSIS REPORT:\
+"""
+REPORT_PROMPT = PromptTemplate.from_template(REPORT_PROMPT_TEMPLATE)
