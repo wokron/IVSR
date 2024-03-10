fb605cc92e3fbdf289245a5fde02edb90e8dd40e
wokron <stringcatwok@gmail.com>
2024-03-10 09:47:14 +0800

    update api and reqs

--- app/api.py
+++ app/api.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, UploadFile
+from typing import Annotated
+from fastapi import APIRouter, Depends, UploadFile
+from app.config import Settings, get_settings
 from app.ml import ml_classify_malware
 
 from app.reqs import (
@@ -13,27 +15,51 @@ api = APIRouter(prefix="/api")
 
 
 @api.post("/file", response_model=UploadResult)
-async def upload_file(file: UploadFile):
-    hash = await mobsf_upload_apk(file.filename, file.file.read())
+async def upload_file(
+    settings: Annotated[Settings, Depends(get_settings)],
+    file: UploadFile,
+):
+    hash = await mobsf_upload_apk(
+        settings.mobsf_url,
+        settings.mobsf_secret,
+        file.filename,
+        file.file.read(),
+    )
     return {"hash": hash}
 
 
 @api.get("/scan", response_model=ScanResult)
 async def scan_file(
+    settings: Annotated[Settings, Depends(get_settings)],
     hash: str,
 ):
-    mobsf_resp = await mobsf_scan_file(hash)
+    mobsf_resp = await mobsf_scan_file(
+        settings.mobsf_url,
+        settings.mobsf_secret,
+        hash,
+    )
     resp = ScanResult.from_mobsf(mobsf_resp)
     return resp
 
 
 @api.get("/source/{hash}/{file_path:path}", response_model=File)
-async def view_source_file(hash: str, file_path: str):
-    mobsf_resp = await mobsf_download_source_file(hash, file_path)
+async def view_source_file(
+    settings: Annotated[Settings, Depends(get_settings)],
+    hash: str,
+    file_path: str,
+):
+    mobsf_resp = await mobsf_download_source_file(
+        settings.mobsf_url,
+        settings.mobsf_secret,
+        hash,
+        file_path,
+    )
     return mobsf_resp
 
 
 @api.post("/scan/ml")
-async def scan_file_ml(file: UploadFile):
+async def scan_file_ml(
+    file: UploadFile,
+):
     result = await ml_classify_malware(file.filename, file.file.read())
     return result
--- app/reqs.py
+++ app/reqs.py
@@ -1,5 +1,5 @@
 import httpx
-from pydantic import FilePath
+from pydantic import AnyHttpUrl, FilePath
 from fastapi import HTTPException
 
 from app.config import get_settings
@@ -11,13 +11,16 @@ from app.schemas.mobsf import (
 )
 
 
-async def mobsf_upload_apk(file_name: str, file_data: bytes):
-    settings = get_settings()
-
+async def mobsf_upload_apk(
+    mobsf_url: AnyHttpUrl,
+    mobsf_secret: str,
+    file_name: str,
+    file_data: bytes,
+):
     async with httpx.AsyncClient() as client:
         resp = await client.post(
-            str(settings.mobsf_url) + "api/v1/upload",
-            headers={"Authorization": settings.mobsf_secret},
+            str(mobsf_url) + "api/v1/upload",
+            headers={"Authorization": mobsf_secret},
             files={"file": (file_name, file_data)},
         )
 
@@ -30,22 +33,22 @@ async def mobsf_upload_apk(file_name: str, file_data: bytes):
     return UploadResponse.model_validate_json(resp.content).hash
 
 
-async def mobsf_download_apk(hash: str):
-    settings = get_settings()
-
+async def mobsf_download_apk(mobsf_url: AnyHttpUrl, hash: str):
     async with httpx.AsyncClient() as client:
-        resp = await client.get(str(settings.mobsf_url) + f"download/{hash}.apk")
+        resp = await client.get(str(mobsf_url) + f"download/{hash}.apk")
 
     return resp.read()
 
 
-async def mobsf_scan_file(hash: str):
-    settings = get_settings()
-
+async def mobsf_scan_file(
+    mobsf_url: AnyHttpUrl,
+    mobsf_secret: str,
+    hash: str,
+):
     async with httpx.AsyncClient() as client:
         resp = await client.post(
-            str(settings.mobsf_url) + "api/v1/scan",
-            headers={"Authorization": settings.mobsf_secret},
+            str(mobsf_url) + "api/v1/scan",
+            headers={"Authorization": mobsf_secret},
             data={"hash": hash},
             timeout=300,
         )
@@ -59,13 +62,16 @@ async def mobsf_scan_file(hash: str):
     return ScanResponse.model_validate_json(resp.content)
 
 
-async def mobsf_download_source_file(hash: str, file: FilePath):
-    settings = get_settings()
-
+async def mobsf_download_source_file(
+    mobsf_url: AnyHttpUrl,
+    mobsf_secret: str,
+    hash: str,
+    file: FilePath,
+):
     async with httpx.AsyncClient() as client:
         resp = await client.post(
-            str(settings.mobsf_url) + "api/v1/view_source",
-            headers={"Authorization": settings.mobsf_secret},
+            str(mobsf_url) + "api/v1/view_source",
+            headers={"Authorization": mobsf_secret},
             data={"hash": hash, "file": file, "type": "apk"},
         )
 
